885eb8a4d5a74d6cb90742c39ab27f026dd3ad0f
Ngoc Khuat <qn.khuat@gmail.com>
2022-11-02 15:11:37 +0700

    some macro notes

--- macro.lisp
+++ macro.lisp
@@ -69,3 +69,130 @@ a
 (our-dolist
   (x '(1 2 3))
   (print x))
+
+
+;; A cruel definition of `defmacro`
+
+(defmacro our-expander (name) `(get ,name 'expander))
+
+(defmacro our-defmacro
+  (name params &body body)
+  (let ((g (gensym)))
+    `(progn
+       (setf (our-expander ',name)
+             #'(lambda (,g)
+                 (block ,name
+                        (destructuring-bind ,params (cdr ,g)
+                          ,@body))))
+       ',name)))
+
+(defun our-defmacroexpand-1 (expr)
+  (if (and (consp expr) (our-expander (car expr)))
+    (funcall (our-expander (car expr)) expr)
+    expr))
+
+(defun inc
+  (x)
+  (+ 1 x))
+
+(our-defmacro
+  our-add-1
+  (x y)
+  `(+ (inc ,x) (inc ,y)))
+
+
+(our-add-1 3 4)
+
+
+(let ((a 1))
+  (setq a 2 b a)
+  (list a b))
+;; => (2 2)
+
+
+(let ((a 1))
+  (psetq a 2 b a)
+  (list a b))
+;; => (2 1)
+;; psetq is different form setq in that psetq set things in parallel
+;; this psetq is primarily used to write macro `do`
+
+(defmacro our-do (bindforms (test &rest result) &body body)
+  (let ((label (gensym)))
+    `(prog ,(make-init-forms bindforms)
+           ,label
+           (if ,test
+             (return (progn ,@result)))
+           ,@body
+           (psetq ,@(make-stepforms bindforms))
+           (go ,label))))
+
+(defun make-init-forms (bindforms)
+  (mapcar #'(lambda (b)
+              (if (consp b)
+                (list (car b) (cadr b))
+                (list b nil)))
+          bindforms))
+
+
+(defun make-stepforms (bindforms)
+  (mapcan #'(lambda (b)
+              (if (and (consp b) (third b))
+                (list (car b) (third b))
+                nil))
+          bindforms))
+
+(do ((x 0 (+ x 1)))
+  ((= x 3) "FOUND IT")
+  (print x))
+
+
+;; some note about prog and its variation
+(progn
+  (print 1)
+  (print 2)
+  2)
+;; => 2
+;; `progn` works like `do` in clojure, it returns the last expression, Thus the `n`
+(prog1
+  1
+  (print 1)
+  (print 2)
+  2)
+;; => 1
+;; `prog1` and prog2 returns the 1st and second expressions respectively
+
+(prog ((x 0))
+      redo (if (= 3 x)
+             (return "DONE"))
+      (print x)
+      (setq x (+ x 1))
+      (print x)
+      (go redo))
+
+;; prog is a bit special, it supports the return and labeling
+
+(mapcar #'(lambda (x) (+ 1 x)) '(2 3 4))
+
+;; one note with dependence on Macros is that when we re-define a macro, any functions that were expanded with the old macro will stay the same. If we want those to be re-defined ,we need to re-evaluate it.
+
+
+
+(defmacro
+  do-if (x yes no)
+  `(if ,x
+     ,yes
+     ,no))
+
+(do-if nil
+       (print "YES")
+       (print "NO"))
+
+(defmacro simple-add
+  (x)
+  (+ 1 x))
+
+(macroexpand '(do-if nil
+                     (simple-add 3)
+                     (simple-add 4)))
+
